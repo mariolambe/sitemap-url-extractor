f33f602a4422f6d9e62a40fdaebefe12c5e1190e
mariolambe <61537859+mariolambe@users.noreply.github.com>
2024-08-05 17:05:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,27 @@ html = html.replace("<head>", """<head>
 
 index.write_text(html)
 
+
+
+# JavaScript to inject meta tags into the head
+inject_meta_tags = """
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var head = document.head;
+
+    var metaDescription = document.createElement('meta');
+    metaDescription.name = 'description';
+    metaDescription.content = 'Extract URLs from sitemap XML files with this easy-to-use Streamlit app. Enter the URL of a sitemap XML file and get all contained URLs.';
+    head.appendChild(metaDescription);
+
+    var metaAdsense = document.createElement('meta');
+    metaAdsense.name = 'google-adsense-account';
+    metaAdsense.content = 'ca-pub-2331172121439147';
+    head.appendChild(metaAdsense);
+});
+</script>
+"""
+
 # Inject the JavaScript into the Streamlit app
 components.html(inject_meta_tags, height=0)
 
@@ -70,37 +91,4 @@ def autofill_and_submit():
 
 st.button("Try this Google Sitemap", on_click=autofill_and_submit)
 
-col1, col2 = st.columns([3, 1])
-with col1:
-    sitemap_url = st.text_input('Enter Sitemap URL:', placeholder="Enter your sitemap URL here", key='sitemap')
-    go_button_clicked = st.button('Go', key='go_button')
-
-    # Check if 'Go' button is clicked or autofill was triggered
-    if go_button_clicked or st.session_state.go:
-        with st.spinner('Extracting URLs...'):
-            urls = extract_urls_from_sitemap(sitemap_url)
-            st.session_state.go = False  # Reset the 'go' state
-        if urls:
-            st.success(f'Extracted {len(urls)} URLs:')
-            st.text_area('Extracted URLs', '\n'.join(urls), height=200)
-
-            # Download button
-            st.download_button(label='Download URLs', data='\n'.join(urls), file_name='urls.txt', mime='text/plain', key='download')
-        else:
-            st.warning("No URLs found or there was an error with the sitemap.")
-
-# Hide Streamlit's default menu and footer
-hide_default_format = """
-       <style>
-       #MainMenu {visibility: hidden; }
-       footer {visibility: hidden;}
-       </style>
-       """
-st.markdown(hide_default_format, unsafe_allow_html=True)
-
-
-
-
-
-
-
+col1,
