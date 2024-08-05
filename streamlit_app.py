5604abd9cc349b08363ecf8462f663162fc0d34c
mariolambe <61537859+mariolambe@users.noreply.github.com>
2024-08-05 15:29:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,24 @@ from bs4 import BeautifulSoup
 
 def extract_urls_from_sitemap(sitemap_url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-    response = requests.get(sitemap_url, headers=headers, timeout=60)
-    soup = BeautifulSoup(response.content, 'xml')
-    urls = []
-    for loc in soup.find_all('loc'):
-        urls.append(loc.text)
-    return urls
+    try:
+        response = requests.get(sitemap_url, headers=headers, timeout=60)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'xml')
+        urls = [loc.text for loc in soup.find_all('loc')]
+        return urls
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error fetching the sitemap: {e}")
+        return []
+
+# Inject meta title and description
+meta_tags = """
+<head>
+<title>Sitemap URL Extractor [Free SEO] - SEO Tool</title>
+<meta name="description" content="Extract URLs from sitemap XML files with this easy-to-use Streamlit app. Enter the URL of a sitemap XML file and get all contained URLs.">
+</head>
+"""
+st.markdown(meta_tags, unsafe_allow_html=True)
 
 st.set_page_config(page_title="Sitemap URL Extractor", page_icon=":memo:", layout="wide")
 st.header('Sitemap URL Extractor :sunglasses:')
@@ -24,37 +36,37 @@ Simply enter the URL of a sitemap XML file, click "Go", and the app will extract
 It works also with sitemap index file (example: https://www.google.com/sitemap.xml). 
 
 If you want to explore alternative ways (for example via Google sheets, Screaming Frog, command line) check out this article on [**How to extract urls from sitemaps**](https://www.mariolambertucci.com/how-to-extract-urls-from-sitemaps/)
-
 """
 
 # Add sidebar text
 st.sidebar.markdown(SIDEBAR_TEXT)
 
-#Define sidebar subheader text
-SIDERBAR_SUBHEADER_TEXT = """
+# Define sidebar subheader text
+SIDEBAR_SUBHEADER_TEXT = """
 About Author
 SEO Specialist [**Mario Lambertucci**](https://www.linkedin.com/in/mariolambertucci/)
 """
 
 # Add sidebar subheader text
-st.sidebar.subheader(SIDERBAR_SUBHEADER_TEXT)
+st.sidebar.subheader(SIDEBAR_SUBHEADER_TEXT)
 
-
+# Main content
 col1, col2 = st.columns([2, 1])
 with col1:
     sitemap_url = st.text_input('Enter sitemap URL:', key='sitemap')
     if st.button('Go', key='go'):
-        urls = extract_urls_from_sitemap(sitemap_url)
-        st.write(f'Extracted {len(urls)} URLs:')
-        download_button = st.download_button(label='Download URLs', data='\n'.join(urls), file_name='urls.txt', mime='text/plain', key='download')
-        if download_button:
-            st.write('Download successful.')
-        with st.container():
+        with st.spinner('Extracting URLs...'):
+            urls = extract_urls_from_sitemap(sitemap_url)
+        if urls:
+            st.write(f'Extracted {len(urls)} URLs:')
             st.code('\n'.join(urls), language='')
-        download_button = st.download_button(label='Download URLs', data='\n'.join(urls), file_name='urls.txt', mime='text/plain', key='download1')
-        if download_button:
-            st.write('Download successful.')
-            
+
+            # Download button
+            st.download_button(label='Download URLs', data='\n'.join(urls), file_name='urls.txt', mime='text/plain', key='download')
+        else:
+            st.warning("No URLs found or there was an error with the sitemap.")
+
+# Hide Streamlit's default menu and footer
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
@@ -62,3 +74,4 @@ hide_default_format = """
        </style>
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
+
