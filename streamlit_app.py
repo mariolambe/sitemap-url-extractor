e598c692a57a97f068357a661b149a66081fa9a5
mariolambe <61537859+mariolambe@users.noreply.github.com>
2024-09-21 21:33:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,111 +1,12 @@
 import streamlit as st
-import streamlit.components.v1 as components
-import requests
-from bs4 import BeautifulSoup
 
-# Set page configuration (this should be at the top)
-st.set_page_config(page_title="Sitemap URL Extractor", page_icon="😎", layout="wide")
+st.set_page_config(page_title="Redirecting...", page_icon="🔄")
 
-# Custom component for redirection
-def redirect_to_netlify():
-    components.html(
-        f"""
-        <script>
-        window.addEventListener('load', function() {{
-            window.location.href = 'https://sitemap-url-extractor.netlify.app/';
-        }});
-        </script>
-        """,
-        height=0,
-    )
+st.markdown("""
+    <meta http-equiv="refresh" content="0; URL=https://sitemap-url-extractor.netlify.app/">
+    <script>
+        window.location.href = "https://sitemap-url-extractor.netlify.app/";
+    </script>
+""", unsafe_allow_html=True)
 
-# Check for redirection parameter
-params = st.experimental_get_query_params()
-if 'redirect' in params and params['redirect'][0] == 'true':
-    redirect_to_netlify()
-else:
-    # Your original Streamlit app code goes here
-    def extract_urls_from_sitemap(sitemap_url):
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-        try:
-            response = requests.get(sitemap_url, headers=headers, timeout=60)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.content, 'xml')
-            urls = [loc.text for loc in soup.find_all('loc')]
-            return urls
-        except requests.exceptions.RequestException as e:
-            st.error(f"Error fetching the sitemap: {e}")
-            return []
-
-    # Include Google Adsense tracking code
-    with open("google_adsense.html", "r") as f:
-        html_code = f.read()
-        components.html(html_code, height=0)
-
-    st.title('Sitemap URL Extractor 😎')
-
-    # Define sidebar text
-    SIDEBAR_TEXT = """
-    ### About the Sitemap URL Extractor
-    The Sitemap URL Extractor is a Streamlit app that helps you extract URLs from sitemap XML files.
-    It works also with sitemap index file (example: [Google Sitemap](https://www.google.com/sitemap.xml)).
-    If you want to explore alternative ways (for example via Google sheets, Screaming Frog, command line) check out this article on [**How to extract URLs from sitemaps**](https://www.mariolambertucci.com/how-to-extract-urls-from-sitemaps/).
-    """
-
-    # Add sidebar text
-    st.sidebar.markdown(SIDEBAR_TEXT)
-
-    # Define sidebar subheader text
-    SIDEBAR_SUBHEADER_TEXT = """
-    ### About Author
-    SEO Specialist [**Mario Lambertucci**](https://www.linkedin.com/in/mariolambertucci/)
-    """
-
-    # Add sidebar subheader text
-    st.sidebar.subheader(SIDEBAR_SUBHEADER_TEXT)
-
-    # Initialize session state
-    if 'go' not in st.session_state:
-        st.session_state['go'] = False
-
-    # Main content
-    st.write('Enter the URL of your sitemap XML file below and click "Go" to extract all URLs.')
-
-    # Function to handle automatic fill and submit
-    def autofill_and_submit():
-        st.session_state.sitemap = "https://www.google.com/sitemap.xml"
-        st.session_state.go = True
-
-    st.button("Try this Google Sitemap", on_click=autofill_and_submit)
-
-    col1, col2 = st.columns([3, 1])
-    with col1:
-        sitemap_url = st.text_input('Enter Sitemap URL:', placeholder="Enter your sitemap URL here", key='sitemap')
-        go_button_clicked = st.button('Go', key='go_button')
-
-        # Check if 'Go' button is clicked or autofill was triggered
-        if go_button_clicked or st.session_state.go:
-            with st.spinner('Extracting URLs...'):
-                urls = extract_urls_from_sitemap(sitemap_url)
-                st.session_state.go = False  # Reset the 'go' state
-            if urls:
-                st.success(f'Extracted {len(urls)} URLs:')
-                st.code('\n'.join(urls), language='')
-                # Download button
-                st.download_button(label='Download URLs', data='\n'.join(urls), file_name='urls.txt', mime='text/plain', key='download')
-            else:
-                st.warning("No URLs found or there was an error with the sitemap.")
-
-    # Hide Streamlit's default menu and footer
-    hide_default_format = """
-           <style>
-           #MainMenu {visibility: hidden; }
-           footer {visibility: hidden;}
-           </style>
-           """
-    st.markdown(hide_default_format, unsafe_allow_html=True)
-
-    # Add a button to redirect to Netlify
-    if st.button("Go to Netlify App"):
-        st.experimental_set_query_params(redirect='true')
-        st.experimental_rerun()
+st.write("If you are not redirected automatically, follow this [link to the new site](https://sitemap-url-extractor.netlify.app/).")
